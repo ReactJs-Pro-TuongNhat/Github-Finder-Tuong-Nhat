334fbfae381b104316f6ec8cf1348e49aad55596
tuongnhatnguyenvan <tuongnhatnv@gamail.com>
2024-05-19 02:13:29 +0700

    Refactor: Create Home component

--- src/App.js
+++ src/App.js
@@ -2,23 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
-import Search from "./components/users/Search";
-import About from "./components/pages/About";
-import User from "./components/users/User";
-import NotFound from "./components/pages/NotFound";
+import Home from "./components/layout/Home";
+
 const App = () => {
   return (
     <div className="App">
       <Router>
         <Navbar />
-        <div className="container">
-          <Routes >
-            <Route exact path="/" element={<Search />} />
-            <Route exact path="/about" element={<About />} />
-            <Route exact path="/user/:id" element={<User />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes >
-        </div>
+        <Home />
       </Router>
     </div>
   );
---
+++ src/components/layout/Home.js
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Search from "../users/Search";
+import About from "../pages/About";
+import User from "../users/User";
+import NotFound from "../pages/NotFound";
+
+const Home = () => {
+    return(
+        <div className="container">
+          <Routes >
+            <Route exact path="/" element={<Search />} />
+            <Route exact path="/about" element={<About />} />
+            <Route exact path="/user/:id" element={<User />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes >
+        </div>
+    );
+}
+export default Home;
